e1ce188191e36681f3aa1cc2cbf59f565a243025
Tyco-dev <66248196+Tyco-dev@users.noreply.github.com>
2020-06-30 19:38:14 -0400

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%9p42xu%k!^(nuh(#a$wlqtjxj59@9%%wa&=uo6(i+3(%^_rkw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
