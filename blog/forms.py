ec97d8507e1abe4b79c95fac2f761a7d2c1e2e99
Tyco <tillintj@gmail.com>
2020-07-01 19:26:00 -0400

    Holy crap alot happened. I added the ability to comment and instead of the user having to type there name...if you are logged in, your username is automatically tied to the comment. In addition, I added the ability to add tags to posts and also show similar posts by similar tags.

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from .models import Comment
+from django.contrib.auth.models import User
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
+    name = forms.CharField()
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '%9p42xu%k!^(nuh(#a$wlqtjxj59@9%%wa&=uo6(i+3(%^_rkw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'taggit',
 
 
 ]
