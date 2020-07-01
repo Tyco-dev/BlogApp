e7a5c6525d57759256247cc5368a76a8f0796b41
Tyco <tillintj@gmail.com>
2020-07-01 08:36:59 -0400

    Added login/logout/registration functionality using Django default auth. Also added a nav bar with the login/logout/register link.

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '%9p42xu%k!^(nuh(#a$wlqtjxj59@9%%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My Apps
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 
 
     # Django Apps
